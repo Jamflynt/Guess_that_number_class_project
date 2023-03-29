c4564709efdc82bb84c54969c8e04d8db34a9c54
Jamie Flynt <jamflynt@gmail.com>
2023-03-29 11:11:51 -0400

    Defined all variables to be used within project at this stage

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // Check the link in Console in Chrome Dev Tools
 // console.log("Hello World");
 
+// Cael's Cat variable
+let cat = "😸";
+
 // Guess That Number
+// Message to be used throughout the project
+const enterNumText = 'Please enter a number greater than zero:';
+
+// For restarting the game
+let restartGame = true;
+
+// For storing the range of the number to be guessed
+let rangeNum;
+
+// For storing the number of attempts the user has left
+let lives; 
+
+// For storing the user's guess
+let guess;
+
+// For storing the user's response to play again or not play again
+let playAgain;
